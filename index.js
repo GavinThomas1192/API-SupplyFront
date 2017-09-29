e46bccb685b35bfa0e55113b813ac6b2fcf68705
GavinThomas1192 <gthomas1192@gmail.com>
2017-09-29 09:11:26 -0700

    almost working. getting not authorized

--- index.js
+++ index.js
@@ -7,16 +7,22 @@ $(document).ready(function($){
 
     superagent.post('https://api-supply.herokuapp.com/api/signup')
       .send({
-        name: 'FrontEndName',
-        username: 'FrontEndUserName',
+        name: 'testing',
+        username: 'thesearegonnaneedtobedeleted',
         password: 'ILoveCodez',
         email: 'testtest.com',
         subscribedToEmail: true,
         isAdmin: true,
       })
-      .then(res => this.res = res)
-      .catch(console.error);
+      .then(res => {
+        this.res = res;
+        superagent.get('https://api-supply.herokuapp.com/api/newapi/getall')
+          .set('Authorization', `Bearer ${this.res}`)
+          .then(body => {
+            this.body = body;
+            console.log(this.body);
+          })
+          .catch(console.error);
+      });
   });
-  console.log(this.res);
-
 });
