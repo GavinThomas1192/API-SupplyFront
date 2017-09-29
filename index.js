14a4a71274ee42c5aa7b8b10083671be6fefa8cb
GavinThomas1192 <gthomas1192@gmail.com>
2017-09-29 11:19:45 -0700

    finished smaple buttom

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 'use-strict';
 var superagent = superagent;
+// var faker = faker;
+//
+// var firstName = faker.name.firstName();
+//
+// var lastName = faker.name.lastName();
 
+
+let name = Math.random();
+let username = Math.random();
 $(document).ready(function($){
-
+  let token = '';
+  let allapi = '';
   $('#signUp' ).click(function() {
 
     superagent.post('https://api-supply.herokuapp.com/api/signup')
       .send({
-        name: 'testing',
-        username: 'thesearegonnaneedtobedeleted',
+        name: `${name++}`,
+        username: `${username++}`,
         password: 'ILoveCodez',
         email: 'testtest.com',
         subscribedToEmail: true,
         isAdmin: true,
       })
       .then(res => {
-        this.res = res;
+        token = res;
+        console.log(token.text);
         superagent.get('https://api-supply.herokuapp.com/api/newapi/getall')
-          .set('Authorization', `Bearer ${this.res}`)
-          .then(body => {
-            this.body = body;
-            console.log(this.body);
+          .set('Authorization', `Bearer ${token.text}`)
+          .then(res => {
+            allapi = res;
+            console.log(allapi.body);
+            allapi.body.map((api) => {
+              $('#response').append(`<li>${JSON.stringify(api)}</li>`);
+            });
           })
           .catch(console.error);
       });
